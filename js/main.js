802835f63c9726a87aff4a49a4d5e1e584446fa0
Abdullah-git-hub <arafat200423@gmail.com>
2021-09-13 20:21:50 +0600

    opp

--- js/main.js
+++ js/main.js
@@ -1,37 +1,18 @@
-$(".owl-carousel").owlCarousel({
-    items: 4,
-    margin:15,
-    nav: true,
-    // loop:true,
-    // autoplay:true,
-    autoplayTimeout:1500,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1,
-        },
-        580:{
-            items:2,
-        },
-        780:{
-            items:3,
-        },
-        1000:{
-            items:3,
-        },
-        1200:{
-            items:4
-        }
+$("#problemContainer").html($("#phyProbs").html());
+
+
+function tab(p){
+    if(p == 'phy'){
+
+        $("#problemContainer").html($("#phyProbs").html());
+        $("#recPhy").addClass("tabSelected");
+        $("#recMath").removeClass("tabSelected");
+
+    }else if(p == 'math'){
+        
+        $("#problemContainer").html($("#mathProbs").html());
+        $("#recPhy").removeClass("tabSelected");
+        $("#recMath").addClass("tabSelected");
+
     }
-});
-
-// code for side nav bar
-
-$(".sideTrgr").click(function(){
-    $(".sideNabBar").toggleClass("sideInactive");
-})
-
-$(".sideSubMenuHead").click(function(){
-    $(".sideSubMenu").slideToggle(300);
-    $("#sideDownArrow").toggleClass("rotate");
-})
+}
